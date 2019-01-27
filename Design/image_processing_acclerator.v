9a5d54e381c93f61509e5361193aa824b9a32f08
itaiyifrach <itai.yifrach@gmail.com>
2019-01-27 11:02:47 +0200

    [UPDATE] top

--- Design/image_processing_acclerator.v
+++ Design/image_processing_acclerator.v
@@ -1,5 +1,5 @@
 `include processor.v
-`include bmp_arbiter.v
+`include bmp_arbiterBFF.v
 
 
 module image_processing_acclerator (
@@ -43,52 +43,65 @@ module image_processing_acclerator (
   
   // master
   output wire						mstr0_cmplt;
-  input reg							mstr0_rdy;
+  input reg							mstr0_ready;
   output wire [DATA_WIDTH-1:0] 		mstr0_data;
   output [1:0]						mstr0_data_valid;
   
   // internal variables
   wire [DATA_WIDTH-1:0]				data_to_processor;
-  wire 								valid_to_processor;
+  wire 								data_vld_to_processor;
   wire 								done_to_processor;
   wire [DATA_WIDTH-1:0]				data_to_arbiter;
+  wire								data_vld_to_arbiter;
   
   
   
   
   // create the arbiter
-  bmp_arbiter #(DATA_BUS_SIZE = DATA_WIDTH) arbiter (
-	.clk					(clk),
-	.rst_n					(rst_n),
-	.slv0_mode				(slv0_mode),
-	.slv0_data_valid		(slv0_data_valid),
-	.slv0_proc_val			(slv0_proc_val),
-	.slv0_data				(slv0_data),
-	.slv0_rdy				(slv0_rdy),
-	.slv1_mode				(slv1_mode),
-	.slv1_data_valid		(slv1_data_valid), 
-	.slv1_proc_val			(slv1_proc_val),
-	.slv1_data				(slv1_data),
-	.slv1_rdy				(slv1_rdy),
-	.mstr0_ready			(mstr0_rdy),
-	.data_to_processor		(data_to_processor),
-	.data_to_master			(mstr0_data),
-	.valid					(valid_to_processor),
-	.done					(done_to_processor)
+  bmp_arbiterBFF #(DATA_BUS_SIZE = DATA_WIDTH) arbiter (
+	.clk				(clk),
+	.rst_n				(rst_n),
+	.mode				(mode),
+	.data_proc			(data_proc),
+	
+	
+	.slv0_mode			(slv0_mode),
+	.slv0_data_valid	(slv0_data_valid),
+	.slv0_data			(slv0_data),
+	.slv0_data_proc		(slv0_data_proc),
+	
+	.slv1_mode			(slv1_mode),
+	.slv1_data_valid	(slv1_data_valid), 
+	.slv1_data			(slv1_data),
+	.slv1_data_proc		(slv1_data_proc),
+	
+	.mstr0_ready		(mstr0_ready),
+	.mstr0_data_valid	(mstr0_data_valid),
+	// complete to master??
+	
+	.data_out_pr		(data_to_arbiter),
+	.vld_pr				(data_vld_to_arbiter),
+	
+	.data_to_processor	(data_to_processor),
+	.data_to_master		(data_to_master),
+	.valid				(data_vld_to_processor),
+	.done				(done_to_processor)		// is it done to processor (i.e last_data)?
 	);
-  
+
+	
   // create the processor
   processor #(DATA_WIDTH = DATA_WIDTH) processor (
 	.clk			(clk),
     .rst_n			(rst_n),
     .vld			(valid_to_processor),
-    //.last_data		(done_to_processor),
+    .last_data		(done_to_processor),
     .mode			(mode),
     .proc_val		(proc_val),
     .data_in		(data_to_processor),
-    .data_out		(data_to_arbiter)
+    .data_out		(data_to_arbiter),
+	.data_out_vld	(data_vld_to_arbiter),
+	//.done			()
 	);
 	
-
 	
 endmodule : image_processing_acclerator
